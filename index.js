710ee3c8d0e963caa50a2c2e2cbcf38259c2f4bb
NibrasseHmidi <nibrasse.hmidi@gmail.com>
2021-10-11 19:58:19 +0100

    2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const date =(req,res,next) =>{
 const datee = new Date();
 const day = datee.getDay();
 const hours = datee.getHours();
-((day < 1 && day > 0) && (hours < 18 && hours >= 9)) ?
+((day < 5 && day > 0) && (hours < 18 && hours >= 9)) ?
 next() : res.send('we are closed')
 }
 // init middeleware
